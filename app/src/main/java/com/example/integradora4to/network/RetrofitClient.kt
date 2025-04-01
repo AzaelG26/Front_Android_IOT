cdc35ddd31434ea2df4df2c0326512246c2782d3
AzaelG26 <azaelgarcia096@gmail.com>
2025-04-01 17:09:03 -0600

    boton de refresh (no funcional aun)

--- app/src/main/java/com/example/integradora4to/network/RetrofitClient.kt
+++ app/src/main/java/com/example/integradora4to/network/RetrofitClient.kt
@@ -3,11 +3,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-<<<<<<< HEAD
     private const val BASE_URL="http://192.168.122.170:3000/"
-=======
-    private const val BASE_URL = "http://192.168.252.130:3000/"
->>>>>>> 9169f819197c1737559a093d91dcc4b3de55303a
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
