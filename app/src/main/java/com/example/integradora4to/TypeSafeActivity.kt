275263e632016d65397e34bdc2bbec44d904800c
AzaelG26 <azaelgarcia096@gmail.com>
2025-04-04 08:07:19 -0600

    cambios

--- app/src/main/java/com/example/integradora4to/TypeSafeActivity.kt
+++ app/src/main/java/com/example/integradora4to/TypeSafeActivity.kt
@@ -3,13 +3,33 @@ package com.example.integradora4to
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.integradora4to.adapters.BoxListAdapter
 import com.example.integradora4to.databinding.ActivityTypeOfSafeBinding
+import com.example.integradora4to.models.request.response.Box
+import com.example.integradora4to.network.RetrofitClient
+import com.example.integradora4to.repositories.SafeRepository
+import com.example.integradora4to.ui.LoginViewModel
+import com.example.integradora4to.ui.LoginViewModelFactory
+import com.example.integradora4to.ui.SafeViewModel
+import com.example.integradora4to.ui.SafeViewModelFactory
 
 
 class TypeOfSafeActivity : AppCompatActivity(){
     private lateinit var binding: ActivityTypeOfSafeBinding
+    private val safeViewModel: SafeViewModel by viewModels {
+        SafeViewModelFactory(SafeRepository(RetrofitClient.apiService, applicationContext))
+    }
+    private val loginViewModel: LoginViewModel by viewModels { LoginViewModelFactory(applicationContext) }
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: BoxListAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +41,30 @@ class TypeOfSafeActivity : AppCompatActivity(){
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
+        recyclerView = findViewById(R.id.recyclerViewBoxes)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = BoxListAdapter(emptyList())
+        recyclerView.adapter = adapter
+
+        safeViewModel.navigateToLogin.observe(this) { navigate ->
+            if (navigate == true) {
+                logOut()
+            }
+        }
+
+        safeViewModel.boxData.observe(this) { response ->
+            response?.let { updateList(it.box ?: emptyList()) }
+        }
+
+        safeViewModel.errorMessage.observe(this) { errorMsg ->
+            errorMsg?.let {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        safeViewModel.getBoxByUserId()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean{
@@ -37,4 +81,17 @@ class TypeOfSafeActivity : AppCompatActivity(){
         startActivity(intent)
         finish() // Se cierra CreateSafeActivity
     }
+
+    private fun updateList(boxes: List<Box>) {
+        adapter.updateData(boxes)
+    }
+
+    private fun logOut() {
+        loginViewModel.logOut()
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+    }
+
 }
--- app/src/main/java/com/example/integradora4to/network/RetrofitClient.kt
+++ app/src/main/java/com/example/integradora4to/network/RetrofitClient.kt
@@ -3,7 +3,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL="https://b3c4-177-244-54-50.ngrok-free.app/"
+    private const val BASE_URL="http://192.168.100.10:3000/"
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
